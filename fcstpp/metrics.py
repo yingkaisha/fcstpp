2a2fc1c31e3b2638d3ab478041cb285484625846
Yingkai Sha <yingkaisha@gmail.com>
2021-02-01 19:46:50 -0700

    CRPS_1d_nan

--- fcstpp/metrics.py
+++ fcstpp/metrics.py
@@ -109,6 +109,60 @@ def CRPS_2d(y_true, y_ens, land_mask='none'):
     return CRPS, MAE, SPREAD
 
 @nb.njit(fastmath=True)
+def CRPS_1d_nan(y_true, y_ens):
+    '''
+    Given one-dimensional ensemble forecast, compute its CRPS and corresponded two-term decomposition.
+    
+    CRPS, MAE, pairwise_abs_diff = CRPS_1d(y_true, y_ens)
+    
+    Grimit, E.P., Gneiting, T., Berrocal, V.J. and Johnson, N.A., 2006. The continuous ranked probability score 
+    for circular variables and its application to mesoscale forecast ensemble verification. Quarterly Journal of 
+    the Royal Meteorological Society, 132(621C), pp.2925-2942.
+    
+    Input
+    ----------
+        y_true: a numpy array with shape=(time, grids) and represents the (observed) truth 
+        y_pred: a numpy array with shape=(time, ensemble_members, grids), represents the ensemble forecast
+    
+    Output
+    ----------
+        CRPS: the continuous ranked probability score, shape=(time, grids)
+        MAE: mean absolute error
+        SPREAD: pairwise absolute difference among ensemble members (not the spread)
+        
+    '''
+    N_day, EN, N_grids = y_ens.shape
+    M = 2*EN*EN
+    
+    # allocate outputs
+    MAE = np.empty((N_day, N_grids),); MAE[...] = np.nan
+    SPREAD = np.empty((N_day, N_grids),); SPREAD[...] = np.nan
+    
+    # loop over grid points
+    for n in range(N_grids):
+        # loop over days
+        for day in range(N_day):
+            # if obs is nan, then mark result as nan
+            if np.isnan(y_true[day, n]):
+                MAE[day, n] = np.nan
+                SPREAD[day, n] = np.nan
+            else:
+                # calc MAE
+                MAE[day, n] = np.mean(np.abs(y_true[day, n]-y_ens[day, :, n]))
+                # calc SPREAD
+                spread_temp = 0
+                for en1 in range(EN):
+                    for en2 in range(EN):
+                        spread_temp += np.abs(y_ens[day, en1, n]-y_ens[day, en2, n])
+                SPREAD[day, n] = spread_temp/M
+            
+    CRPS = MAE-SPREAD
+    
+    return CRPS, MAE, SPREAD
+
+
+
+@nb.njit(fastmath=True)
 def BS_binary_1d(y_true, y_ens):
     '''
     
